8ab50eab3bd1301f0cfaeffe1b4b22bfc1a54550
tessfbs <tessfbs@gmail.com>
2023-02-28 15:18:57 -0500

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log('DOM is ready to be manipulated with jQuery')
+});
+
+let counterOutput = $("#counter");
+let text = $("#tweet-text")
+counterOutput.text(140)
+
+$("#tweet-text").on('input', function() {
+    counterOutput.text(140 - this.value.length);
+    if(counterOutput.text() < 0){
+      counterOutput.css("color", "red");
+    } else {
+      counterOutput.css("color", ""); // reset to default color if not less than 0
+    }
+    console.log("Number of characters", this.value.length);
+});
+
+
